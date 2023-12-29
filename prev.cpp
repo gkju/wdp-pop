7c94122f1f423382d095ebab7a3d97cdb3967120
gjus <gkjuszczyk@gmail.com>
2023-12-29 18:06:51 +0100

    BUGFIX, add brute with pretty printing

--- prev.cpp
+++ prev.cpp
@@ -2,30 +2,38 @@
 #include <memory>
 #include <climits>
 
-struct Node {
+class Node {
+public:
+    std::shared_ptr<Node> left, right = nullptr;
     int value;
-    std::shared_ptr<Node> left, right;
+
+    Node(int val) : value(val) {}
 };
 
 std::vector<std::shared_ptr<Node>> roots;
 const int MIN = INT_MIN;
 const int MAX = INT_MAX;
 
-std::shared_ptr<Node> descend_l(std::shared_ptr<Node> &node) {
+std::shared_ptr<Node> descend_l(const std::shared_ptr<Node> &node) {
     if (node == nullptr) {
         return nullptr;
     }
     return node->left;
 }
 
-std::shared_ptr<Node> descend_r(std::shared_ptr<Node> &node) {
+std::shared_ptr<Node> descend_r(const std::shared_ptr<Node> &node) {
     if (node == nullptr) {
         return nullptr;
     }
     return node->right;
 }
 
-void set_pom(int query_val, std::pair<int,int> query, std::pair<long long int,long long int> my_range, std::shared_ptr<Node> node, std::shared_ptr<Node> old_node) {
+long long int get_mid(long long int lo, long long int hi) {
+    // dla border case gdy bierzemy srednia z dwoch kolejnych ujemnych liczb
+    return lo +  (hi - lo) / 2;
+}
+
+void set_pom(int query_val, std::pair<int,int> query, std::pair<long long int,long long int> my_range, const std::shared_ptr<Node> &node, const std::shared_ptr<Node> &old_node) {
     if(old_node != nullptr) {
         node->left = old_node->left;
         node->right = old_node->right;
@@ -39,9 +47,10 @@ void set_pom(int query_val, std::pair<int,int> query, std::pair<long long int,lo
     if (query.first > my_range.second || query.second < my_range.first) {
         return;
     }
-    long long int mid = (my_range.first + my_range.second) / 2;
-    node->left = std::make_shared<Node>();
-    node->right = std::make_shared<Node>();
+
+    long long int mid = get_mid(my_range.first, my_range.second);
+    node->left = std::make_shared<Node>(-1);
+    node->right = std::make_shared<Node>(-1);
 
     set_pom(query_val, query, {my_range.first, mid}, descend_l(node), descend_l(old_node));
     set_pom(query_val, query, {mid + 1, my_range.second}, descend_r(node), descend_r(old_node));
@@ -49,11 +58,11 @@ void set_pom(int query_val, std::pair<int,int> query, std::pair<long long int,lo
 }
 
 void set(int value, int lo, int hi) {
-    roots.push_back(std::make_shared<Node>());
+    roots.push_back(std::make_shared<Node>(-1));
     set_pom(value, {lo, hi}, {MIN, MAX}, roots.back(), roots.size() > 1 ? roots[roots.size() - 2] : nullptr);
 }
 
-int query_pom(std::pair<int,int> query, std::pair<long long int,long long int> my_range, std::shared_ptr<Node> node) {
+int query_pom(std::pair<int,int> query, std::pair<long long int,long long int> my_range, const std::shared_ptr<Node> &node) {
     if (node == nullptr) {
         return -1;
     }
@@ -63,11 +72,12 @@ int query_pom(std::pair<int,int> query, std::pair<long long int,long long int> m
     if (query.first > my_range.second || query.second < my_range.first) {
         return -1;
     }
-    long long int mid = (my_range.first + my_range.second) / 2;
+
+    long long int mid = get_mid(my_range.first, my_range.second);
     return std::max(query_pom(query, {my_range.first, mid}, descend_l(node)), query_pom(query, {mid + 1, my_range.second}, descend_r(node)));
 }
 
-int query(int lo, int hi, std::shared_ptr<Node> &node) {
+int query(int lo, int hi, const std::shared_ptr<Node> &node) {
     return query_pom({lo, hi}, {MIN, MAX}, node);
 }
 
